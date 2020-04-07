1e284d7680f09ecf3159a51dcbabf5da72335f52
lennygin <lennginzan@gmail.com>
2020-04-07 18:51:54 -0500

    adding AND, OR

--- controller.c
+++ controller.c
@@ -5,20 +5,44 @@
 #include "model.h"
 #include "view.h"
 
-
 int main()
 {
-    int op, num_inputs, inputs;
+    int op, num_inputs, temp = 0;
 
     op = 0;
     initial_menu();
     tipoElemento *Primero = NULL;
-    main_menu(&Primero, &op, &num_inputs, &inputs);   
-//  InsertaInicio(&Primero);
-    Imprime(Primero);
-    final_menu();
+
+    bla(&num_inputs);
+    inputs(&Primero, &num_inputs);
+    while (temp != 6)
+    {
+        temp = main_menu(&num_inputs);
+
+        switch (temp)
+        {
+        case 1:
+            OR(Primero);
+            Imprime(Primero);
+            break;
+        case 2:
+            AND(Primero);
+            Imprime(Primero);
+            break;
+        case 3:
+            XOR(Primero);
+            Imprime(Primero);
+            break;
+        case 4:
+            NOT(Primero);
+            Imprime(Primero);
+            break;
+        case 5:
+            XNOR(Primero);
+            Imprime(Primero);
+            break;
+        }
+        final_menu();
+    }
     return 0;
 }
-
-
-//printf("op %d\nnum_inputs %d\ninputs %d\n", op, num_inputs, inputs);
